8f1e48ce869c59ba79970ac33e7cf2eab442805c
taufiksamantha-web <taufiksamantha@gmail.com>
2025-11-18 23:00:27 +0700

    Add files via upload

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ErrorInfo, ReactNode } from 'react';
+import React, { ErrorInfo, ReactNode } from 'react';
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 import { supabase } from '../lib/supabaseClient';
 
@@ -10,7 +10,7 @@ interface State {
   hasError: boolean;
 }
 
-class ErrorBoundary extends Component<Props, State> {
+class ErrorBoundary extends React.Component<Props, State> {
   public state: State = {
     hasError: false
   };
